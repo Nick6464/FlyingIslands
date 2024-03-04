4f552dbc7e51558abbf3cdec8afe7e0426b9b485
Nick6464 <49857434+Nick6464@users.noreply.github.com>
2024-03-04 15:42:27 +1300

    Made the noise exponential on the underside which makes spikes and more interesting shapes possible

--- src/main/java/net/nick6464/flyingislands/item/custom/IslandSpawnerItem.java
+++ src/main/java/net/nick6464/flyingislands/item/custom/IslandSpawnerItem.java
@@ -9,15 +9,16 @@ import net.nick6464.flyingislands.FlyingIslands;
 
 public class IslandSpawnerItem extends Item {
 
-    static int ISLAND_SIZE = 30;
+    static int ISLAND_SIZE = 50;
+    static int ISLAND_GROUND_HEIGHT = 50;
     static int ISLAND_RADIUS = ISLAND_SIZE / 2;
     static int ISLAND_CONTAINER_SIZE = (int) (ISLAND_SIZE * 1.5);
     static int ISLAND_CONTAINER_RADIUS = ISLAND_CONTAINER_SIZE / 2;
-    static int ISLAND_GROUND_HEIGHT = 15;
     static int SEED = 12345;
-    static float FREQUENCY = 0.1f;
-    static float MAGNITUDE = 2f;
-    static int RADIAL_DETAIL_MULTIPLIER = ISLAND_RADIUS /  3;
+    static float FREQUENCY = 0.15f;
+    static float GROUND_LAYER_MAGNITUDE = 2f;
+    static float UNDERSIDE_MAGNITUDE = 2f;
+    static int RADIAL_DETAIL_MULTIPLIER = Math.round((float) ISLAND_RADIUS /  3f);
     static int SMOOTHING_PASSES = 2;
 
     public IslandSpawnerItem(Settings settings) {
@@ -80,7 +81,25 @@ public class IslandSpawnerItem extends Item {
         // array under the ground layer, a value of 4 means there are 4 blocks under the ground layer
         for (int x = 0; x < ISLAND_CONTAINER_SIZE; x++) {
             for (int z = 0; z < ISLAND_CONTAINER_SIZE; z++) {
-                for (int y = ISLAND_GROUND_HEIGHT; y > ISLAND_GROUND_HEIGHT - (int) underside[x][z]; y--) {
+                // Ensure there is a block in the ground layer
+                if (!groundLayer[x][z]) {
+                    continue;
+                }
+
+                // Generate noise to add to the depth of the island
+                float noise =
+                        OpenSimplex2S.noise2(SEED, x * FREQUENCY, z * FREQUENCY) * UNDERSIDE_MAGNITUDE * (underside[x][z]/ 3);
+                // Start at the ground level and go down to the underside depth then add a layer
+                // of noise which can be positive or negative
+
+                // Determine the depth of the underside and ensure it's within the valid range
+                int depth = Math.round( ISLAND_GROUND_HEIGHT + 1 - (int) underside[x][z] + noise);
+
+                if (depth < 0) {
+                    depth = 0;
+                }
+
+                for (int y = ISLAND_GROUND_HEIGHT; y >= depth; y--) {
                     blocks[x][y][z] = true;
                 }
             }
@@ -151,7 +170,7 @@ public class IslandSpawnerItem extends Item {
         for (int angle = 0; angle < blockCount; angle++) {
             // Determine the distance from the center of the circle to the block
             float noise = OpenSimplex2S.noise2(SEED, 1, (double) angle / RADIAL_DETAIL_MULTIPLIER * FREQUENCY);
-            double distance = ISLAND_RADIUS + noise * MAGNITUDE;
+            double distance = ISLAND_RADIUS + noise * GROUND_LAYER_MAGNITUDE;
 
             // Determine the angle in radians
             double angleRads = angle * 2 * Math.PI / blockCount;
@@ -160,8 +179,6 @@ public class IslandSpawnerItem extends Item {
             int x = (int) Math.round(distance * Math.cos(angleRads));
             int z = (int) Math.round(distance * Math.sin(angleRads));
 
-            FlyingIslands.LOGGER.info("x: " + x + " z: " + z);
-
             // Adjust the coordinates to ensure they fall within the valid range of the array indices
             x += ISLAND_CONTAINER_RADIUS;
             z += ISLAND_CONTAINER_RADIUS;
@@ -169,6 +186,9 @@ public class IslandSpawnerItem extends Item {
             // Set the block at (x, z) to true if it's within the valid range
             if (x >= 0 && x < ISLAND_CONTAINER_SIZE && z >= 0 && z < ISLAND_CONTAINER_SIZE) {
                 noisyCircle[x][z] = true;
+                FlyingIslands.LOGGER.info("x: " + x + " z: " + z);
+            } else {
+                FlyingIslands.LOGGER.warn("x: " + x + " z: " + z + " is out of range");
             }
         }
     }
@@ -207,13 +227,20 @@ public class IslandSpawnerItem extends Item {
                     if (noisyCircle[x][z]) {
                         // Check if the block is a hard corner and set it to false if it is
                         // Ensure both the positive and negative are done to avoid a corner being missed
+
+                        // Check if the coordinates would be in range
+                        if (x - 2 < 0 || x + 2 >= ISLAND_CONTAINER_SIZE || z - 2 < 0 || z + 2 >= ISLAND_CONTAINER_SIZE) {
+                            continue;
+                        }
+
+
                         if (x - 2 > 0 && noisyCircle[x - 2][z] && noisyCircle[x - 1][z] && noisyCircle[x][z - 2] && noisyCircle[x][z - 1]) {
                             noisyCircle[x][z] = false;
                         }
-                        if (x + 2 < ISLAND_CONTAINER_SIZE && noisyCircle[x + 2][z] && noisyCircle[x + 1][z] && noisyCircle[x][z + 2] && noisyCircle[x][z + 1]) {
+                        if (x - 2 > 0 && noisyCircle[x - 2][z] && noisyCircle[x - 1][z] && noisyCircle[x][z + 2] && noisyCircle[x][z + 1]) {
                             noisyCircle[x][z] = false;
                         }
-                        if (x - 2 > 0 && noisyCircle[x - 2][z] && noisyCircle[x - 1][z] && noisyCircle[x][z + 2] && noisyCircle[x][z + 1]) {
+                        if (x + 2 < ISLAND_CONTAINER_SIZE && noisyCircle[x + 2][z] && noisyCircle[x + 1][z] && noisyCircle[x][z + 2] && noisyCircle[x][z + 1]) {
                             noisyCircle[x][z] = false;
                         }
                         if (x + 2 < ISLAND_CONTAINER_SIZE && noisyCircle[x + 2][z] && noisyCircle[x + 1][z] && noisyCircle[x][z - 2] && noisyCircle[x][z - 1]) {
